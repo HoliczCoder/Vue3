0ec79d4163270e1aa6327744a5fa0679428818cf
Minh <minhtranconglis@gmail.com>
2022-09-06 22:50:00 +0700

    dev

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,143 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <header>
-    <div class="bg-black w-full h-20 flex flex-row justify-center items-center">
-      <div class="w-3/4 h-2/3  overflow-hidden flex flex-row justify-start  align-middle">
+    <div class="bg-black w-full h-24 flex flex-row justify-center items-center">
+      <div
+        class="w-3/4 h-3/4 overflow-hidden flex flex-row justify-start align-middle"
+      >
         <img
           alt="Vue logo"
           class="object-cover scale-90"
           src="https://dd5394a0b8ca8e97ba29-abf76f3d91a2125517d6c7c409f095c7.ssl.cf1.rackcdn.com/content/common/home/logo_yamaha_rev.png"
         />
+        <div
+          class="w-full h-full border-white border-l-2 flex flex-col justify-center"
+        >
+          <div
+            class="flex flex-row border-white border-b-2 justify-between w-full"
+          >
+            <div
+              class="text-white pl-10 uppercase flex flex-row justify-between"
+            >
+              <div class="pr-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                  />
+                </svg>
+              </div>
+              find a dealer
+            </div>
+            <div class="text-white uppercase flex flex-row justify-between">
+              <div class="pr-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                  />
+                </svg>
+              </div>
+              my yamaha
+            </div>
+            <div class="text-white uppercase flex flex-row justify-between">
+              <div class="pr-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                  />
+                </svg>
+              </div>
+              event & demos
+            </div>
+            <div class="text-white uppercase flex flex-row justify-between">
+              <div class="pr-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+                  />
+                </svg>
+              </div>
+              destination yamaha
+            </div>
+            <div
+              class="text-white pr-10 uppercase flex flex-row justify-between"
+            >
+              <div class="pr-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              search
+            </div>
+          </div>
+          <div class="flex flex-row justify-between w-full">
+            <div class="text-white pl-10 uppercase font-semibold text-lg">
+              motorcycle
+            </div>
+            <div class="text-white uppercase font-semibold text-lg">atv</div>
+            <div class="text-white uppercase font-semibold text-lg">
+              side-by-side
+            </div>
+            <div class="text-white uppercase font-semibold text-lg">
+              snow mobile
+            </div>
+            <div class="text-white pr-10 uppercase font-semibold text-lg">
+              power product
+            </div>
+          </div>
+        </div>
       </div>
-      
     </div>
     <!-- <div class="wrapper">
       <HelloWorld msg="You did it!" />
